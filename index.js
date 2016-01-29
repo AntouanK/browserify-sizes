38af506432282a3f7430da3e97770df3f018ab28
Antonis Karamitros @antouank <ablackjim228@gmail.com>
2016-01-29 16:47:47 +0000

    [master] [1.1.0] - make AST selector code more accurate ( it was including duplicates before ) - print totals - sort by name added

--- index.js
+++ index.js
@@ -5,37 +5,70 @@ var program     = require('commander');
 var falafel     = require('falafel');
 var prettyBytes = require('pretty-bytes');
 var fs          = require('fs');
+var packageJson = require(__dirname + '/package.json');
 var modules     = [];
 
-program.version('1.0.0')
-  .option('--source-dir [path]', 'The source code directory')
-  .option('--source-file [file]', 'The path to the JS bundle produced by browserify')
+program.version(packageJson.version)
+  .option('--source-dir   [path]', 'The source code directory')
+  .option('--source-file  [file]', 'The path to the JS bundle produced by browserify')
   .option('--sort-by-size', 'Sort output by size')
+  .option('--sort-by-name', 'Sort output by name')
   .parse(process.argv);
 
 
-var sourceDir = program.sourceDir;
-var sourceFile = program.sourceFile;
+//  capture the arguments
+var sourceDir   = program.sourceDir;
+var sourceFile  = program.sourceFile;
 
-if(typeof sourceDir !== 'string'){
+//  counters for maximum string sizes of the values
+//  ( needed for alignment )
+var maxNameLen = 0;
+var maxSourceLengthLen = 0;
+var totalBundlesLength = 0;
+
+//  throw if invalid arguments
+if(typeof sourceDir !== 'string' || sourceDir.length < 1){
   throw new Error('source-dir is missing');
 }
-if(typeof sourceFile !== 'string'){
+if(typeof sourceFile !== 'string' || sourceFile.length < 1){
   throw new Error('source-file is missing');
 }
 
+//  print arguments for verification
 console.log('source-dir: ', sourceDir);
 console.log('source-file: ', sourceFile);
 
+// visual separator
+var separator =
+'----------------------------------------'+
+'----------------------------------------'
+
+//  load source file
 var src = fs.readFileSync(sourceFile);
 console.log(
   'Source code length: ', src.length,
   '  (size) ', prettyBytes(src.length)
 );
+console.log(separator);
 
+//  parse code and capture information on each module bundled
 falafel(src, function (node) {
 
   if(typeof node.raw === 'string' && node.raw.match(sourceDir)){
+
+    //  check if parent is a property
+    if(node.parent.type !== 'Property'){
+      return true;
+    }
+    //  check if parent's value has an Array as value
+    if(node.parent.value.type !== 'ArrayExpression'){
+      return true;
+    }
+    // check if first element has 3 elements itself
+    if(node.parent.value.elements[0].params.length !== 3){
+      return true;
+    }
+
     var sourceLength = node.parent.source().length;
     modules.push({
       name: node.raw.replace(sourceDir, ''),
@@ -46,14 +79,15 @@ falafel(src, function (node) {
 
 });
 
-var maxNameLen = 0;
-var maxSourceLengthLen = 0;
 
+//  sort results if needed
 if(program.sortBySize){
+  //  sort by sourceLength
   modules =
   modules
   .sort(function sortFn(a, b) {
 
+    //  convert them to numbers
     var sizeA = +a.sourceLength;
     var sizeB = +b.sourceLength;
 
@@ -66,19 +100,23 @@ if(program.sortBySize){
     return 0;
   });
 }
+else if(program.sortByName){
+  //  sort by sourceLength
+  modules =
+  modules
+  .sort(function sortFn(a, b) {
 
-modules
-.map(function prepareModuleInfo(m) {
-  if(m.name.length > maxNameLen){
-    maxNameLen = m.name.length;
-  }
+    if(a.name > b.name){
+      return -1
+    }
+    if(a.name < b.name){
+      return 1
+    }
+    return 0;
+  });
+}
 
-  if(m.sourceLength.length > maxSourceLengthLen){
-    maxSourceLengthLen = m.sourceLength.length;
-  }
-  return m;
-})
-.forEach(function printModuleInfo(m) {
+var printModuleInfo = function printModuleInfo(m) {
 
   var name = m.name;
   var sourceLength = '' + m.sourceLength;
@@ -94,4 +132,48 @@ modules
     '| Length: ', sourceLength,
     '| Size: ', m.prettySize
   );
+};
+
+
+modules
+//  capture maximum string lengths
+.map(function prepareModuleInfo(m) {
+
+  totalBundlesLength += +m.sourceLength;
+
+  if(m.name.length > maxNameLen){
+    maxNameLen = m.name.length;
+  }
+
+  if(m.sourceLength.length > maxSourceLengthLen){
+    maxSourceLengthLen = m.sourceLength.length + 3;
+  }
+  return m;
 })
+// for each module, print it's row
+.forEach(printModuleInfo);
+
+
+
+function printTotals() {
+
+  console.log(separator);
+
+  var totalModules = 'Modules: ' + modules.length;
+  var totalModulesBlanks = maxNameLen - totalModules.length;
+  while(totalModulesBlanks--){ totalModules+= ' '; }
+
+  var _totalBundlesLength = '' + totalBundlesLength;
+  var totalBundlesBlanks = maxSourceLengthLen - _totalBundlesLength.length;
+  while(totalBundlesBlanks--){ _totalBundlesLength += ' '; }
+
+
+  console.log(
+    totalModules,
+    '| Length: ', _totalBundlesLength,
+    '| Size: ', prettyBytes(totalBundlesLength)
+  );
+}
+
+//  print totals row
+printTotals();
